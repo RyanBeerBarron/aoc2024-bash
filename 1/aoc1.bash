6e1768eff042df9d8ffe90c709ff3e9cbbc09b89
Ryan Barron <ryanbar1997@gmail.com>
2024-12-02 21:17:50 +0100

    Added another solution to day 1 problem without using `rm` and `mkfifo`
    But a comment with the separate solution
    Also modified the main solution so that it takes it's input from stdin
    instead of having the filename hard coded

--- 1/aoc1.bash
+++ 1/aoc1.bash
@@ -1,20 +1,45 @@
 mkfifo first_col second_col first_col_sorted second_col_sorted
-while read first second
-do
-    echo "$first" >&3
-    echo "$second" >&4
-done 3>first_col 4>second_col <input1 &
 
 sort -g <first_col >first_col_sorted &
 sort -g <second_col >second_col_sorted &
 
-(( total = 0 ))
-while read -u 3 first && read -u 4 second
-do
-    (( diff = first - second ))
-    (( diff = diff < 0 ? -diff : diff ))
-    (( total += diff ))
-done 3<first_col_sorted 4<second_col_sorted
-echo "$total"
+{
+    (( total = 0 ))
+    while read -u 3 first && read -u 4 second
+    do
+        (( diff = first - second ))
+        (( diff = diff < 0 ? -diff : diff ))
+        (( total += diff ))
+    done
+    echo "$total"
+} 3<first_col_sorted 4<second_col_sorted &
 
+while read first second
+do
+    echo "$first" >&3
+    echo "$second" >&4
+done 3>first_col 4>second_col
+
+wait
 rm first_col second_col first_col_sorted second_col_sorted
+
+#################################################
+# Solution without using `mkfifo` and `rm`      #
+# Less elegant but uses less external commands  #
+#################################################
+
+# input_file="$1"
+# exec 3< <(while read first ignored; do echo $first; done < "$input_file" | sort)
+# exec 4< <(while read ignored second; do echo $second; done < "$input_file" | sort)
+#
+# (( total = 0 ))
+# while read -u 3 first && read -u 4 second
+# do
+#     (( diff = first - second ))
+#     (( diff = diff < 0 ? -diff : diff ))
+#     (( total += diff ))
+# done
+# echo "$total"
+#
+# exec 3<&-
+# exec 4<&-
