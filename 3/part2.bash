813777bb876d35248f1f88532ec3307c9fc3da30
Ryan Barron <ryanbar1997@gmail.com>
2024-12-04 01:17:31 +0100

    Day 3: Using remove prefix and regex with quantifier
    Learned about a nice trick to check if a string begins/ends with a certain prefix/suffix
    I already knew about the '#' and '%' operation inside parameter expansion
    But simply to check the condition, we can use to operators and verify that the result is different from the original string
    as such:
    
    ```bash
    var="my_string"
    if test "$var" != "${var#*_}"
    then
        # do stuff
    fi
    ```
    
    If `var` contains a prefix matching the pattern '*_', the RHS of '!=' will be different from the LHS, the original string
    If it does not contain, the remove prefix expansion won't remove anything and the RHS will simply be `"${var}"`

---
+++ 3/part2.bash
@@ -0,0 +1,38 @@
+
+read -rd '' input
+cleaned_input=""
+(( do = 1 ))
+while true
+do
+    if (( do == 1 ))
+    then
+        if test "$input" != "${input#*don\'t()}"
+        then
+            cleaned_input="${cleaned_input}${input%%don\'t()*}"
+            input="${input#*don\'t()}"
+            (( do = 0 ))
+        else
+            break
+        fi
+    else
+        if test "$input" != "${input#*do()}"
+        then
+            input="${input#*do()}"
+            (( do = 1 ))
+        else
+            break
+        fi
+    fi
+done
+
+total=0
+while test "$cleaned_input" != "${cleaned_input#*mul}"
+do
+    cleaned_input="${cleaned_input#*mul}"
+    if [[ "$cleaned_input" =~ ^\(([0-9]{1,3}),([0-9]{1,3})\) ]]
+    then
+        (( product = ${BASH_REMATCH[1]} * ${BASH_REMATCH[2]} ))
+        (( total += product ))
+    fi
+done
+echo "$total"
